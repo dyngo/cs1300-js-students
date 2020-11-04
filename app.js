f38c22cfdd6aef7a9663231f71c1795887518353
dyngo <60231589+dyngo@users.noreply.github.com>
2020-11-04 16:54:09 -0500

    d

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "djzJbE805xyTGGPQJvlfohGxTCZYokUXTT1DiLc3cJA";
+var apiToken = "?token=djzJbE805xyTGGPQJvlfohGxTCZYokUXTT1DiLc3cJA";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -21,33 +21,46 @@ const corsPromise = () =>
   });
 
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
-corsPromise().then(
-  (request) =>
-    (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
-);
+
+function randPlant() {
+  corsPromise().then(
+    (request) =>
+      (request.onload = request.onerror = function () {
+        const response = request.response;
+        const data = JSON.parse(response).data;
+        const randomElement = Math.floor(Math.random() * data.length);
+        document.getElementById('genus').innerHTML = data[randomElement].genus
+        console.log(data[randomElement].image_url);
+        document.getElementById("image").src = data[randomElement].image_url;
+
+      })
+  );
+
+}
+
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
 
-const displayDiv = () => {
-  const wrapper = document.createElement("div");
-  wrapper.setAttribute("id","wrapper");
-  const header = document.createElement("h1");
-  header.innerText = "My h1";
-  wrapper.appendChild(header);
-  document.getElementById("plants").appendChild(wrapper);
-};
+// const displayDiv = () => {
+//   const wrapper = document.createElement("div");
+//   wrapper.setAttribute("id","wrapper");
+//   const header = document.createElement("h1");
+//   header.innerText = "My h1";
+//   wrapper.appendChild(header);
+//   document.getElementById("plants").appendChild(wrapper);
+// };
 
 
 
-const handleResponse = (requestResponse) => {
-  const jsonified = JSON.parse(requestResponse);
-  const plantsArray = jsonified.data;
+// const handleResponse = (requestResponse) => {
+//   const jsonified = JSON.parse(requestResponse);
+//   const plantsArray = jsonified.data;
 
-  const plantsAfter1753 = plantsArray.filter((arrayItem) => {
-    return arrayItem.year > 1753;
-  })
+//   const plantsAfter1753 = plantsArray.filter((arrayItem) => {
+//     return arrayItem.year > 1753;
+//   })
 
-  plantsAfter1753.map(console.log)
-};
+//   plantsAfter1753.map(console.log)
+// };
+
+
